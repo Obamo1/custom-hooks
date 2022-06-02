7319190f1072b2af9d50af5c40e98514f3149c5e
obamo1 <joobamo@gmail.com>
2022-06-02 20:43:23 +0100

    neew

---
+++ build/precache-manifest.95cdcf00348b0927e1f46a3bf16657a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c07ecccdc8134b13d20e3b911a4aab69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90ef5dd61357e1df9494",
+    "url": "/static/css/main.5ca41be2.chunk.css"
+  },
+  {
+    "revision": "c0489e16b6aa2ccd5328",
+    "url": "/static/js/2.a560e0c4.chunk.js"
+  },
+  {
+    "revision": "90ef5dd61357e1df9494",
+    "url": "/static/js/main.0dd79ce6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
